46104a0850daea19c7c876615ba72fabf575b300
david om <rahrahfez@hotmail.com>
2021-06-24 12:31:48 -0400

    misc

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,20 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { Store } from '@ngrx/store';
 
 import { LoginComponent } from './login.component';
-import { MaterialModule } from '../../material/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { JwtModule } from '@auth0/angular-jwt';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let store: MockStore<{ loggedIn: boolean }>;
-  const initialState = { loggedIn: false };
+
   // const user$: Observable<User> = of({
   //   Username: 'test',
   //   UserId: '12345',
@@ -25,24 +19,11 @@ describe('LoginComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
       imports: [
-        MaterialModule,
-        ReactiveFormsModule,
-        FlexLayoutModule,
+        SharedModule,
         HttpClientTestingModule,
-        JwtModule.forRoot({
-          config: {
-            tokenGetter: () => {
-              return localStorage.getItem('token');
-            },
-            whitelistedDomains: ['localhost:5000'],
-            blacklistedRoutes: []
-          }
-        }),
         NoopAnimationsModule
        ],
-       providers: [
-         provideMockStore({ initialState })
-       ]
+       providers: []
     })
     .compileComponents();
   }));
@@ -50,8 +31,6 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-
-    store = TestBed.get(Store);
 
     fixture.detectChanges();
   });
